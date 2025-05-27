26a4821a432a64f3c1af866ee8b547edbb60b3b3
blacksmith69-glitch <cryptowitharyancourselink@gmail.com>
2025-05-27 20:02:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ else:
 print("""[~]=========================================[~]
 [~]         Cloudfire Captcha Bypass                 [~]
 [~]         youtube :- cryptowithAryan               [~]
-[~]         TG Channel : @cryptowitharyanog         [~]
+[~]         TG Channel : @cryptowitharyanog          [~]
 [~]=========================================         [~]
 """.strip())
 print(f'Web url : \033[0;32mhttp://localhost:{prt}\033[0m')
